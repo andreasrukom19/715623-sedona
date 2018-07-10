97d75f405241041fd4adc4a679ff5baa64ea107b
leysanlatypova <leisan.latypova@mail.ru>
2018-07-11 02:55:59 +0300

    обновление

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,15 @@
     var link = document.querySelector(".hotelsearch-button");
     var popup = document.querySelector(".hotel-searching");
+    var link = document.querySelector(".check-field")
 
     link.addEventListener("click", function (evt) {
       evt.preventDefault();
       popup.classList.toggle("modal-show");
+    });
+
+    link.addEventListener("submit", function (evt) {
+      if (!check-field.value) {
+      evt.preventDefault();
+      popup.classList.add("modal-error");
+    }
     });
