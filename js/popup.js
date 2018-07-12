5f1e1f05cdb4dff45a1f1e9ae6943d2a08cfe270
leysanlatypova <leisan.latypova@mail.ru>
2018-07-12 20:35:53 +0300

    анимация ошибки

--- js/popup.js
+++ js/popup.js
@@ -1,15 +1,23 @@
     var link = document.querySelector(".hotelsearch-button");
     var popup = document.querySelector(".hotel-searching");
-    var field = document.querySelector(".check-field");
+    var field = popup.querySelector("[required]");
 
     link.addEventListener("click", function (evt) {
       evt.preventDefault();
       popup.classList.toggle("modal-show");
+      field.focus();
     });
 
-    field.addEventListener("submit", function (evt) {
-      if (!check-field.value) {
+    popup.addEventListener("submit", function (evt) {
+      if (!field.value) {
       evt.preventDefault();
+      console.log("Ошибка");
       popup.classList.add("modal-error");
     }
+    });
+
+    popup.addEventListener("submit", function (evt) {
+      evt.preventDefault();
+      console.log("Отправляем форму");
+      popup.classList.remove("modal-error");
     });
