ca83428a1e11f7660f0afa49e4d631d9d343f49a
leysanlatypova <leisan.latypova@mail.ru>
2018-07-13 00:11:58 +0300

    исправление анимации

--- js/popup.js
+++ js/popup.js
@@ -10,8 +10,8 @@ link.addEventListener("click", function (evt) {
 popup.addEventListener("submit", function (evt) {
   if (!field.value) {
     evt.preventDefault();
-    popup.classList.add("modal-error");
-  } else {
     popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
   }
 });
